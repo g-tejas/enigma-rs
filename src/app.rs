3c041ebb7aa2ae2ecae52e904535db101c8cdb68
Tejas G <ttejasgarrepally@gmail.com>
2023-01-01 00:11:14 +0800

    cleaned up alot of stuff

--- src/app.rs
+++ src/app.rs
@@ -1,43 +1,32 @@
-use crate::colors;
-use crate::plot::candlestick_chart;
-use crate::utils;
-use crate::widgets::trades::{show, Trades};
-use barter_data::{
-    builder::Streams,
-    model::{subscription::SubKind, MarketEvent},
-    ExchangeId,
-};
-use barter_integration::model::InstrumentKind;
+use crate::{plot::candlestick_chart, utils, widgets::Widget};
+use barter_data::model::MarketEvent;
 use eframe::egui;
 use egui::plot::{Line, Plot, PlotPoints};
-use egui::{CentralPanel, Id, LayerId, TopBottomPanel, Ui, WidgetText};
-use egui_dock::{DockArea, Node, NodeIndex, Style, StyleBuilder, TabViewer, Tree};
-use futures::StreamExt;
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 use std::sync::mpsc::{Receiver, Sender};
 
 pub struct State {
     // HashSet, so only unique widget names are stored.
     // TODO: Add unique identifier to widget names OrderBook::spot::BTCUSD for example.
     // This is so we can have multiple orderbooks up at the same time
-    pub open_tabs: HashSet<String>,
-    pub style: Option<Style>,
-    // pub bestbid: Arc<Mutex<f64>>,
-    pub bestbid: f64,
-    pub tx: Sender<MarketEvent>,
-    pub rx: Receiver<MarketEvent>,
-    pub trade_data: Trades,
+    open_tabs: HashSet<String>,
+    style: Option<egui_dock::Style>,
+    bestbid: f64,
+    tx: Sender<MarketEvent>,
+    rx: Receiver<MarketEvent>,
+    trade_data: VecDeque<MarketEvent>,
+    gizmos: Vec<Box<dyn Widget>>, // Vector of pointers to a trait value Widget
 }
 
-impl TabViewer for State {
+impl egui_dock::TabViewer for State {
     type Tab = String;
 
-    fn ui(&mut self, ui: &mut Ui, tab: &mut Self::Tab) {
+    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
         match tab.as_str() {
             "Welcome" => self.candlestick_chart(ui),
             "Portfolio" => self.line_chart(ui),
             "Machine Configuration" => self.machine_config(ui),
-            "Orderbook" => show(ui, &mut self.trade_data, self.tx.clone()), // from trades crate
+            "Orderbook" => self.gizmos[0].show(ui, &mut self.trade_data, self.tx.clone()), // from trades crate
             _ => {
                 ui.label(tab.as_str());
             }
@@ -45,12 +34,14 @@ impl TabViewer for State {
     }
 
     // when you right click a tab
-    fn context_menu(&mut self, ui: &mut Ui, tab: &mut Self::Tab) {
-        ui.label(tab.as_str());
-        ui.label("This is a context menu");
+    fn context_menu(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
+        match tab.as_str() {
+            "Orderbook" => ui.label("We gon add some fancy shit here"),
+            _ => ui.label("helo"),
+        };
     }
 
-    fn title(&mut self, tab: &mut Self::Tab) -> WidgetText {
+    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
         tab.as_str().into()
     }
 
@@ -64,11 +55,14 @@ impl State {
     // The only things that should be stored here are styling / open_tabs related stuff
     // since the things that can be accessed from self, are very limited. Or we can store the financial
     // data here itself
-    fn candlestick_chart(&mut self, ui: &mut Ui) {
+    fn candlestick_chart(&mut self, ui: &mut egui::Ui) {
         candlestick_chart(ui);
+        egui::Window::new("Hello").show(ui.ctx(), |ui| {
+            ui.label("Hgello world");
+        });
     }
 
-    fn line_chart(&mut self, ui: &mut Ui) {
+    fn line_chart(&mut self, ui: &mut egui::Ui) {
         // ui.label(format!("BTCUSDT = {}", self.bestbid.lock().unwrap()));
         ui.label(format!("BTCUSDT = {}", self.bestbid));
         // delete later
@@ -81,14 +75,6 @@ impl State {
                 ui.label("BTC");
                 ui.label("ETH");
             });
-
-        if ui.button("Connect").clicked() {
-            // match ticker {
-            //     Ticker::BTC => barter(self.tx.clone(), "btc".to_string()),
-            //     Ticker::ETH => barter(self.tx.clone(), "eth".to_string()),
-            // }
-            barter(self.tx.clone(), "eth".to_string());
-        }
         let plot = Plot::new("Measurements");
         let sin: PlotPoints = (0..1000)
             .map(|i| {
@@ -101,7 +87,7 @@ impl State {
         });
     }
 
-    fn machine_config(&mut self, ui: &mut Ui) {
+    fn machine_config(&mut self, ui: &mut egui::Ui) {
         ui.heading("Machine Configuration");
         // let style = self.style.as_mut().unwrap();
 
@@ -115,7 +101,7 @@ impl State {
 
 pub struct Machine {
     pub state: State,
-    pub tree: Tree<String>,
+    pub tree: egui_dock::Tree<String>,
 }
 
 impl Machine {
@@ -138,15 +124,19 @@ impl Machine {
 impl Default for Machine {
     // Default Layout
     fn default() -> Self {
-        let mut tree = Tree::new(vec![
+        let mut tree = egui_dock::Tree::new(vec![
             "Welcome".to_owned(),
             "Machine Configuration".to_owned(),
         ]);
-        let [a, _b] = tree.split_left(NodeIndex::root(), 0.4, vec!["Orderbook".to_owned()]);
+        let [a, _b] = tree.split_left(
+            egui_dock::NodeIndex::root(),
+            0.4,
+            vec!["Orderbook".to_owned()],
+        );
         let [_, _] = tree.split_below(a, 0.5, vec!["Portfolio".to_owned()]);
         let mut open_tabs = HashSet::new();
         for node in tree.iter() {
-            if let Node::Leaf { tabs, .. } = node {
+            if let egui_dock::Node::Leaf { tabs, .. } = node {
                 for tab in tabs {
                     open_tabs.insert(tab.clone());
                 }
@@ -154,7 +144,13 @@ impl Default for Machine {
         }
         let bestbid = 0.;
         let (tx, rx) = std::sync::mpsc::channel();
-        let trade_data = Trades::new();
+        let trade_data = VecDeque::new();
+
+        let gizmos: Vec<Box<dyn Widget>> = vec![
+            Box::new(crate::widgets::aggr_trades::AggrTrades::default()),
+            Box::new(crate::widgets::aggr_trades::AggrTrades::default()),
+        ];
+
         let state = State {
             open_tabs,
             style: None,
@@ -162,6 +158,7 @@ impl Default for Machine {
             tx,
             rx,
             trade_data,
+            gizmos,
         };
         Self { state, tree }
     }
@@ -178,20 +175,11 @@ impl eframe::App for Machine {
         if let Ok(event) = self.state.rx.try_recv() {
             self.state.trade_data.push_front(event);
             self.state.trade_data.truncate(50);
-            // match event.kind {
-            //     DataKind::Trade(trade) => {
-            //         println!("{:?}", trade);
-            //         self.state.trade_data.push_front(trade);
-            //         self.state.trade_data.truncate(50); // works!
-            //     }
-            //     _ => println!("Failed"),
-            // }
-            // self.state.bestbid = bb;
         }
-        TopBottomPanel::top("egui_dock::MenuBar").show(ctx, |ui| {
+
+        egui::TopBottomPanel::top("egui_dock::MenuBar").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
                 egui::widgets::global_dark_light_mode_switch(ui);
-
                 ui.separator();
                 ui.menu_button("⚙", |ui| {
                     if ui.button("Quit").clicked() {
@@ -223,86 +211,50 @@ impl eframe::App for Machine {
                     // }
                     ui.label("This is where we will add the various tabs");
                 });
-                // let style = match self.state.style.as_mut() {
-                //     Some(style) => {
-                //         ui.menu_button("View", |ui| {
-                //             ui.checkbox(&mut style.tabs_are_draggable, "Tabs are draggable");
-                //         });
-                //     }
-                //     None => println!("failed"),
-                // };
-                // self.state.style.as_mut().unwrap();
             })
         });
 
         let panel_config = egui::containers::Frame {
-            fill: colors::COLOR_BLACK,
+            inner_margin: egui::style::Margin {
+                left: 4.,
+                right: 7.,
+                top: 0.,
+                bottom: 3.,
+            },
+            fill: egui::Color32::from_rgba_unmultiplied(25, 25, 25, 200),
             ..Default::default()
         };
 
         // Add the "workspaces feature here" > more deets in the README.md
         egui::TopBottomPanel::bottom("bottom_panel")
+            .exact_height(25.)
             .resizable(false)
             .frame(panel_config)
             .show(ctx, |ui| {
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                    // let mut style = self
-                    //     .state
-                    //     .style
-                    //     .get_or_insert(Style::from_egui(&ui.ctx().style()))
-                    //     .clone();
-                    // ui.checkbox(&mut self.state.style.tabs_are_draggable, "Lock");
-                    use egui::special_emojis::{GITHUB, TWITTER};
-                    ui.hyperlink_to(
-                        egui::RichText::new(TWITTER),
-                        "https://twitter.com/ernerfeldt",
-                    );
-                    ui.hyperlink_to(egui::RichText::new(GITHUB), "https://github.com/emilk/egui");
+                    if ui.selectable_label(true, "🔒").clicked() {
+                        // let style = self.state.style.as_mut().unwrap();
+                        // ui.checkbox(&mut style.tabs_are_draggable, "Lock");
+                        println!("locked");
+                    }
                 });
             });
 
-        CentralPanel::default().show(ctx, |_ui| {
-            let layer_id = LayerId::background();
-            let max_rect = ctx.available_rect();
-            let clip_rect = ctx.available_rect();
-            let id = Id::new("egui_dock::DockArea");
-            let mut ui = Ui::new(ctx.clone(), layer_id, id, max_rect, clip_rect);
-
-            DockArea::new(&mut self.tree)
+        egui::CentralPanel::default().show(ctx, |_ui| {
+            egui_dock::DockArea::new(&mut self.tree)
                 .style(
-                    StyleBuilder::from_egui(ctx.style().as_ref())
-                        // .show_add_buttons(true)
-                        // .show_add_popup(true)
+                    egui_dock::StyleBuilder::from_egui(ctx.style().as_ref())
+                        .show_add_buttons(true)
+                        .show_add_popup(true)
+                        .with_separator_color_hovered(egui::Color32::LIGHT_BLUE)
+                        .with_border_color(egui::Color32::RED)
                         .expand_tabs(true)
                         .build(),
                 )
-                .show_inside(&mut ui, &mut self.state);
-            // .show(ctx, &mut self.state);
+                .show(ctx, &mut self.state);
         });
+
+        // Call repaint every update
         ctx.request_repaint();
     }
-}
-
-// delete later
-fn barter(tx: Sender<MarketEvent>, ticker: String) {
-    tokio::spawn(async move {
-        loop {
-            let streams = Streams::builder()
-                .subscribe([(
-                    ExchangeId::BinanceFuturesUsd,
-                    ticker.as_str(),
-                    "usdt",
-                    InstrumentKind::FuturePerpetual,
-                    SubKind::Trade,
-                )])
-                .init()
-                .await
-                .unwrap();
-            let mut joined_stream = streams.join_map::<MarketEvent>().await;
-
-            while let Some((_exchange, event)) = joined_stream.next().await {
-                let _result = tx.send(event);
-            }
-        }
-    });
 }
