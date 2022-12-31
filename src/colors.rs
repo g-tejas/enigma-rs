39311e0f3d2b60aa081d964cbcd70c314ff5e754
Tejas G <ttejasgarrepally@gmail.com>
2023-01-01 00:11:06 +0800

    unnecessary, so deleted

--- src/colors.rs
+++
@@ -1,4 +0,0 @@
-use eframe::egui::Color32;
-
-pub const COLOR_BLACK: Color32 = Color32::from_rgb(0, 0, 0);
-// pub const COLOR_RED_TRANSPARENT: Color32 = Color32::from_rgba_unmultiplied(255, 0, 0, 125);
