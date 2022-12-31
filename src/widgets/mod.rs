33d74be3f4225b638650eb10fef59c48ab6b140d
Tejas G <ttejasgarrepally@gmail.com>
2023-01-01 00:01:38 +0800

    all widgets must have Widget trait implemented

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,1 +1,20 @@
-pub mod trades;
+pub mod aggr_trades;
+use barter_data::model::MarketEvent;
+use std::collections::VecDeque;
+use std::sync::mpsc::Sender;
+
+// ----------------------------------------------------------------------------
+
+/// All widgets have to implement this trait
+pub trait Widget {
+    /// `&'static` so we can also use it as a key to store open/close state.
+    fn name(&self) -> &'static str;
+
+    /// Show windows, etc
+    fn show(
+        &mut self,
+        ui: &mut eframe::egui::Ui,
+        trade_data: &mut VecDeque<MarketEvent>,
+        tx: Sender<MarketEvent>,
+    );
+}
