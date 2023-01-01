7988e81239cdc3f88c2acca59db5d672833bde56
Tejas G <ttejasgarrepally@gmail.com>
2023-01-01 17:07:09 +0800

    added candle handler

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,5 @@
 use crate::defines::fonts::FONT_PROGGY_CLEAN;
-use barter_data::model::{DataKind, MarketEvent, PublicTrade};
-use barter_integration::model::Side;
+use barter_data::model::{Candle, DataKind, MarketEvent, PublicTrade};
 use eframe::egui::{self, FontData, FontDefinitions};
 use std::io::Error;
 
@@ -18,22 +17,24 @@ pub fn configure_fonts(ctx: &egui::Context) {
     ctx.set_fonts(fonts);
 }
 
-pub fn split_ticker(input: String) -> Result<(String, String), Error> {
-    let parts: Vec<&str> = input.as_str().split("-").collect();
+pub fn split_ticker<'a>(input: &'a str) -> Result<(&'a str, &'a str), Error> {
+    let parts: Vec<&str> = input.split("-").collect();
     if parts.len() != 2 {
         panic!("Invalid ticker");
     }
-    Ok((parts[0].to_string(), parts[1].to_string()))
+    Ok((parts[0], parts[1]))
 }
 
-pub fn get_trade(event: MarketEvent) -> PublicTrade {
+pub fn get_trade(event: MarketEvent) -> Option<PublicTrade> {
     match event.kind {
-        DataKind::Trade(trade) => trade,
-        _ => PublicTrade {
-            id: "123".to_string(),
-            price: 123.,
-            quantity: 123.,
-            side: Side::Buy,
-        },
+        DataKind::Trade(trade) => Some(trade),
+        _ => None,
+    }
+}
+
+pub fn get_candle(event: MarketEvent) -> Option<Candle> {
+    match event.kind {
+        DataKind::Candle(candle) => Some(candle),
+        _ => None,
     }
 }
