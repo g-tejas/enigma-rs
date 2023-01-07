edb43db43435aa7ee4ebc7d2109f496bb357b394
Tejas G <ttejasgarrepally@gmail.com>
2023-01-07 12:21:16 +0800

    new feed for orderbook data

--- src/gateway.rs
+++ src/gateway.rs
@@ -65,6 +65,33 @@ pub fn add_ohlcv(tx: Sender<MarketEvent>, ticker: &'static str) {
     };
 }
 
+pub fn add_orderbook(tx: Sender<MarketEvent>, ticker: &'static str) {
+    if let Ok((base, quote)) = utils::split_ticker(ticker) {
+        println!("Successfully Connected to Orderbook");
+        tokio::spawn(async move {
+            loop {
+                let streams = Streams::builder()
+                    .subscribe([(
+                        ExchangeId::BinanceFuturesUsd,
+                        base,
+                        quote,
+                        InstrumentKind::FuturePerpetual,
+                        SubKind::OrderBook, // Different
+                    )])
+                    .init()
+                    .await
+                    .unwrap();
+                let mut joined_stream = streams.join_map::<MarketEvent>().await;
+
+                while let Some((_exchange, event)) = joined_stream.next().await {
+                    println!("{:?}", event);
+                    let _result = tx.send(event);
+                }
+            }
+        });
+    };
+}
+
 // No way to test cause liquidations are so rare lmao.
 // pub fn add_liqs(tx: Sender<MarketEvent>, ticker: String) {
 //     if let Ok((base, quote)) = utils::split_ticker(ticker) {
