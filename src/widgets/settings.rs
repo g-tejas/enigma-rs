6ddefe22c4c780a577341671872669970dd468e5
Tejas G <ttejasgarrepally@gmail.com>
2023-01-07 12:21:02 +0800

    settings needs to have &mut self mutable borrow since we are editing the settings

--- src/widgets/settings.rs
+++ src/widgets/settings.rs
@@ -29,7 +29,8 @@ impl super::Widget for Settings {
         tx: Sender<MarketEvent>,
         trades: &mut VecDeque<Trade>,
         candles: &mut VecDeque<Candle>,
-        orderbooks: &mut VecDeque<OrderBook>,
+        best_bids: &mut VecDeque<f32>,
+        best_asks: &mut VecDeque<f32>,
         liquidations: &mut VecDeque<Liquidation>,
     ) {
         let Self {
@@ -55,6 +56,10 @@ impl super::Widget for Settings {
             ui.label("Edit shit here");
             // ui.checkbox(&mut style.tabs_are_draggable, "Tabs are draggable");
         });
+    }
+
+    fn settings(&mut self, ui: &mut eframe::egui::Ui) {
+        todo!()
     }
 
     fn context_menu(&self, ui: &mut Ui) {
